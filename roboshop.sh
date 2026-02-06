fe8c0906fd0a327ca7a8ecebadae5efc8320ced4
pramod kumar m <pramodaws4@gmail.com>
2026-02-06 10:19:50 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -5,25 +5,26 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-    instance_id = $( aws ec2 run-instances \
+    instance_id=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t3.micro \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
-    --output table )
+    --output text )
+
     for [ $instance == "frontend" ]; then
     IP=$(
         aws ec2 describe-instances \
-    --instance-ids $instance_id \
-    --query 'Reservations[*].Instances[*].PublicIpAddress' \
-    --output text
+            --instance-ids $instance_id \
+            --query 'Reservations[*].Instances[*].PublicIpAddress' \
+            --output text
     )
     else
     IP=$(
         aws ec2 describe-instances \
-    --instance-ids $instance_id \
-    --query 'Reservations[*].Instances[*].PrivateIpAddress' \
-    --output text
+            --instance-ids $instance_id \
+            --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+            --output text
     )
 
 done
