f16c614c82088b0043b8cc9880e658268e61a6ed
pramod kumar m <pramodaws4@gmail.com>
2026-02-06 10:12:24 +0530

    actions

---
+++ roboshop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SG_ID="sg-00014de2d5b7245ea"    
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do
+    instance_id = $( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output table )
+    for [ $instance == "frontend" ]; then
+    IP=$(
+        aws ec2 describe-instances \
+    --instance-ids $instance_id \
+    --query 'Reservations[*].Instances[*].PublicIpAddress' \
+    --output text
+    )
+    else
+    IP=$(
+        aws ec2 describe-instances \
+    --instance-ids $instance_id \
+    --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+    --output text
+    )
+
+done
