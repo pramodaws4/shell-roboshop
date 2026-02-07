b16fabbd0ceeab47d39115dbb3ad0872a0506987
pramod kumar m <pramodaws4@gmail.com>
2026-02-07 09:54:21 +0530

    actions

---
+++ redies.sh
@@ -0,0 +1,41 @@
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable redis -y
+VALIDATE $? "disable redies" 
+
+dnf module enable redis:7 -y &>>$LOGS_FILE              
+VALIDATE $? "enabling redis virsion 7" 
+
+dnf install redis -y &>>$LOGS_FILE
+VALIDATE $? "Installing Redis server"
+
+sed -i  -e 's/127.0.0.1/0.0.0.0/g' -e '/protedted-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections and disabling protected mode"
+
+systemctl enable redis &>>$LOGS_FILE
+VALIDATE $? "Enable Redis"
+
+systemctl start redis &>>$LOGS_FILE
+VALIDATE $? "Start Redis"
