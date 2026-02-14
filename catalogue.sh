a819f8e86ed700e67d4fab857e2f16fb088fee16
pramod kumar m <pramodaws4@gmail.com>
2026-02-14 08:37:08 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -38,7 +38,7 @@ dnf install nodejs -y &>> $LOG_FILE
 validate $? "installing nodejs "
 
 id roboshop &>> $LOG_FILE
-if [ $? -ne 0]; then 
+if [ $? -ne 0 ]; then 
 useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOG_FILE
 validate $? "adding user  "
 else
